2f54653fba3688a768a931f60e8bfed2cc28ea9a
Mintesnot Girma <MintesnotGi@ethiopianairlines.com>
2023-08-23 17:05:55 +0300

    #addstringbuilder

--- ReadAndWritefFile/ASMReader.cs
+++ ReadAndWritefFile/ASMReader.cs
@@ -18,7 +18,9 @@ namespace ReadAndWritefFile
 
                 try
                 {
-                 var lines=   File.ReadAllLines(file);
+                    var content = string.Empty;
+                    var stringbuild = new StringBuilder();
+                    var lines=   File.ReadAllLines(file);
                     var filename = lines[3].Split(" ")[0];
                     try
                     {
@@ -27,30 +29,43 @@ namespace ReadAndWritefFile
                             if (line.Contains("Created by Movement Manager"))
                             {
                                 var date = line.Split("/");
-                                WriteToFile(date[0], filename);
+                                stringbuild.Append(date[0]);
+                                stringbuild.AppendLine(); // This will add the platform-specific newline sequence
+
                             }
                             else if (line.Contains("OPER"))
                             {
                                 var opr = line.Split(" ");
-                                WriteToFile(opr[0], filename);
+                                stringbuild.Append(opr[0]);
+                                stringbuild.AppendLine(); // This will add the platform-specific newline sequence
+
+                                //WriteToFile(opr[0], filename);
                             }
                             else if (line.Contains("ET") && line.Contains('/'))
                             {
                                 var opr2 = line.Split("/");
                                 if (opr2[1].Length==2)
                                 {
-                                    WriteToFile($"{opr2[0]}/{DateTime.Now.ToString("MMM").ToUpper()}{DateTime.Now.ToString("yyyy")}", filename);
+                                    stringbuild.Append($"{opr2[0]}/{DateTime.Now.ToString("MMM").ToUpper()}{DateTime.Now.ToString("yyyy")}");
+                                    stringbuild.AppendLine(); // This will add the platform-specific newline sequence
+
 
                                 }
                             }
 
                             else
                             {
-                                WriteToFile($"{line}", filename);
+                                stringbuild.Append(line);
+                                stringbuild.AppendLine(); // This will add the platform-specific newline sequence
+
 
                             }
 
                         }
+                      var appendedcontent=  stringbuild.ToString();
+                      
+                        WriteToFile(appendedcontent, filename);
+
                     }
                     catch (Exception)
                     {
